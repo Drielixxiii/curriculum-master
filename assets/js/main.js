576f9454d4b5658edc579ae7fe46f20d4d03c260
Drielixxiii <drieli.freitas286@gmail.com>
2021-07-08 15:41:19 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,20 @@ function scrollActive() {
 }
 
 window.addEventListener("scroll", scrollActive);
+
+// MUDAR BACK DO HEADER
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+
+// MOSTRAR O SCROLL UP
+// function scrollUp() {
+//   const scrollUp = document.getElementById("scroll-up");
+//   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+//   else scrollUp.classList.remove("show-scroll");
+// }
+// window.addEventListener("scroll", scrollUp);
