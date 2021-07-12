19e650d83e7e1a797f3cdf0fd8ef229191b8ba5a
Drielixxiii <drieli.freitas286@gmail.com>
2021-07-12 10:48:20 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,10 +139,11 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader);
 
 
-// MOSTRAR O SCROLL UP
-// function scrollUp() {
-//   const scrollUp = document.getElementById("scroll-up");
-//   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-//   else scrollUp.classList.remove("show-scroll");
-// }
-// window.addEventListener("scroll", scrollUp);
+//SCROLL UP
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-Up class
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
